0570a73e4dac0f5e241b74884be211e00831fde8
Rodafi <53792380+Rodafi@users.noreply.github.com>
2020-06-18 14:36:36 -0300

    Add files via upload
    Comentários adicionados.

--- teste.sql
+++ teste.sql
@@ -29,12 +29,15 @@ CREATE TABLE saldoestoquetotal(
 
 .separator ";"
 .import unisoma_saldo_estoq_full.csv saldoestoquetotal
+/*Elimina header*/
 DELETE FROM saldoestoquetotal WHERE ROWID IN (SELECT ROWID FROM saldoestoquetotal WHERE ROWID  LIMIT 1);
+/*Acerta formatos de alguns números.*/
 UPDATE saldoestoquetotal
 	SET qtde_atual = ROUND(CAST(REPLACE(REPLACE(qtde_atual, ".", ""), ',', '.') AS NUMERIC), 6),
 		qtde_disp = ROUND(CAST(REPLACE(REPLACE(qtde_disp, ".", ""), ',', '.') AS NUMERIC), 6),
 		custo = ROUND(CAST(REPLACE(REPLACE(custo, ".", ""), ',', '.') AS NUMERIC), 6);
 
+/*Importa arquivo separado por tab.*/
 .mode tabs
 .import saldoestoque.txt saldoestoque
 DELETE FROM saldoestoque WHERE ROWID IN (SELECT ROWID FROM saldoestoque WHERE ROWID  LIMIT 1);
